3dc596f1ae40a22f34a7293e12cb0d986ab034d9
tzakabush <148607252+tzakabush@users.noreply.github.com>
2024-12-29 22:09:30 +0300

    basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -6,7 +6,6 @@ addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 # Print the results 
-
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print("Addition of {number1} and {number2} is {addition}")
+print("Subtraction of {number1} and {number2} is {subtraction}")
+print("Multiplication of {number1} and {number2} is {multiplication}")
